6db4694684e3b3b2504124ef023fe20e94987e05
Nick <nwalk34@yahoo.com>
2021-02-19 04:01:54 +0000

    Updated errors within variables and .toLowerCase

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,15 +8,16 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters == 5 kilometers? ", "(5+3)/2*10= ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size of the ISS? "];
+let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters == 5 kilometers? ", "(5+3)/2*10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name: ")
+candidateName = input.question("What is your name?: ")
 }
+
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 for (let i = 0; i < questions.length; i++) {
@@ -30,7 +31,7 @@ function gradeQuiz(candidateAnswers) {
 let numberOfCorrect = 0;
   console.log(`\nCandidate Name: ${candidateName}`);
   for (let i = 0; i < questions.length; i++) {
-    if(candidateAnswers[i].toLowerCase() === correctAnswers[i]) {
+    if(candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
       numberOfCorrect++;
   }
    console.log(`${i +1} ${questions[i]}\nCandidate answer: ${candidateAnswers[i]}\nCorrect answer: ${correctAnswers[i]}`+'\n')
@@ -48,7 +49,7 @@ let numberOfCorrect = 0;
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log(`Greetings ${candidateName}!`);
+  console.log(`Hello: ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
